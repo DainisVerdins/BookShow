5fd48837bc63ce4f493cd386b638cff20b239300
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2023-06-20 15:34:30 +0300

    feat: removed not needed components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Fresh start</h1>
   </main>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({ });
+</script>
+<style lang="scss">
+</style>
